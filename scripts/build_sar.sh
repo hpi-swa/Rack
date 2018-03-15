fd67229a92053e4c38a75b01ce0c6294422c5e1e
Patrick R <patrick.rein@gmail.com>
2018-03-15 09:49:25 -0700

    Fixes loading the squeak run scripts

--- scripts/build_sar.sh
+++ scripts/build_sar.sh
@@ -8,7 +8,7 @@ set -e
 if [[ "${TRAVIS_TAG}" = "v"* ]];
 then
   echo "Starting to build the sar now"
-  ${SMALLTALK_CI_HOME}/squeak/run.sh
+  . ${SMALLTALK_CI_HOME}/squeak/run.sh
   squeak::run_script "prepare_sar.st"  
   cp $SMALLTALK_CI_BUILD/Rack.sar $TRAVIS_BUILD_DIR
 else
