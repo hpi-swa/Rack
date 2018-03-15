18a70613a2e4d2022e6cdadf912bea8ae2481980
Patrick R <patrick.rein@gmail.com>
2018-03-15 08:32:07 -0700

    Initial scripts to create sar

---
+++ scripts/build_sar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2018 - present Software Architecture Group (Hasso Plattner Institute)
+# Copyright (c) 2018 - present Patrick Rein
+
+set -e
+
+
+if [[ "%{TRAVIS_TAG}" == v* ]];
+then
+  "${SMALLTALK_CI_HOME}/squeak/run.sh"
+  squeak::run_script "prepare_sar.st"  
+  cp $SMALLTALK_CI_BUILD/Rack.sar $TRAVIS_BUILD_DIR
+fi
